82f140751b212f89532e58aea2d4295ca6b316df
sidneni <siddhartha.kasaraneni@gmail.com>
2017-10-18 19:19:28 +0200

    name changes and clean up

--- MultilayerPerceptronNetwork.py
+++ MultilayerPerceptronNetwork.py
@@ -2,8 +2,8 @@ import numpy as np
 import nn
 
 def Train_MLP(inputPatterns, targetOutputs, eta = 0.01, architecture = {'hiddenLayers': 4,'respectiveHiddenUnits':[6,5,7,4]}, batchSize = 100, outputClasses = 1):
-    checkData = 10
-    updates = 10**4
+    checkData = 10**2
+    updates = 10**5
     nPatterns = np.size(inputPatterns,axis = 0)
     inputDimensions = np.size(inputPatterns,axis = 1)
     weightMatrix = nn.Initialize_weights(inputDimensions,outputClasses,architecture)
@@ -22,11 +22,14 @@ def Train_MLP(inputPatterns, targetOutputs, eta = 0.01, architecture = {'hiddenL
             testOutput = nn.FeedForward(inputPatterns,weightMatrix,architecture,returnType = 2)
             H = np.array((targetOutputs-testOutput)**2).sum()/(2*nPatterns)
             print(n,' Upadtes completed out of', updates,', Energy: ',H)
+    return weightMatrix
 
 
+def Test_MLP(testPatterns,testOutputs,weightMatrix,architecture = {'hiddenLayers': 4,'respectiveHiddenUnits':[6,5,7,4]}):
+    nPatterns = np.size(testPatterns,axis = 0)
+    output_FeedForward = nn.FeedForward(testPatterns,weightMatrix,architecture,returnType = 2)
+    H = np.array((testOutputs-output_FeedForward)**2).sum()/(2*nPatterns)
+    print('Energy: ',H)
+    return H
 
-
-x = np.random.rand(1000,3)
-y = np.random.rand(1000,4)
-Train_MLP(x,y,eta = 0.001,outputClasses = 4)
 
